7a71e6f90437441f4553bf31f95afeb466454c4f
ShamliKiruba <shamlikiruba@gmail.com>
2020-12-31 15:53:56 +0530

    Updated the layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { layout } from './constants';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>Responsive design</h2>
+			<div className="container">
+				{layout.map(item => {
+					return(
+						<div className="item" style={{ height: item.height }}>
+							<p>{item.index}</p>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,60 @@
+export const layout = [{
+	index: 1,
+	width: 	'200px',
+	height: '300px',
+},
+{
+	index: 2,
+	width: 	'200px',
+	height: '350px',
+},
+{
+	index: 3,
+	width: 	'200px',
+	height: '400px',
+},
+{
+	index: 4,
+	width: 	'200px',
+	height: '270px',
+},
+{
+	index: 5,
+	width: 	'200px',
+	height: '450px',
+},
+{
+	index: 6,
+	width: 	'200px',
+	height: '450px',
+},
+{
+	index: 7,
+	width: 	'200px',
+	height: '300px',
+},
+{
+	index: 8,
+	width: 	'200px',
+	height: '320px',
+},
+{
+	index: 9,
+	width: 	'200px',
+	height: '350px',
+},
+{
+	index: 10,
+	width: 	'200px',
+	height: '300px',
+},
+{
+	index: 11,
+	width: 	'200px',
+	height: '400px',
+},
+{
+	index: 12,
+	width: 	'200px',
+	height: '250px',
+}];
